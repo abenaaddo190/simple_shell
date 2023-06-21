e49190eee178296ef52888ef40ba6fe0f8b70a69
abenaaddo190 <128905696+abenaaddo190@users.noreply.github.com>
2023-06-21 14:53:01 +0000

    Add files via upload

---
+++ ampersand.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+#define MAX_LINE 80 /* The maximum length command */
+
+int main(void)
+{
+    char *args[MAX_LINE/2 + 1]; /* command line arguments */
+    int should_run = 1; /* flag to determine when to exit program */
+    char input[MAX_LINE];
+    int status;
+    pid_t pid;
+
+    while (should_run) {
+        printf("simple_shell> ");
+        fflush(stdout);
+
+        fgets(input, MAX_LINE, stdin);
+        input[strlen(input) - 1] = '\0';
+
+        char *token = strtok(input, " ");
+        int i = 0;
+        while (token != NULL) {
+            args[i] = token;
+            token = strtok(NULL, " ");
+            i++;
+        }
+        args[i] = NULL;
+
+        pid = fork();
+        if (pid < 0) {
+            fprintf(stderr, "Fork failed\n");
+            return 1;
+        } else if (pid == 0) {
+            int j = 0;
+            while (args[j] != NULL) {
+                if (strcmp(args[j], "&&") == 0) {
+                    if (WEXITSTATUS(status) == 0) {
+                        args[j] = NULL;
+                        execvp(args[0], args);
+                    } else {
+                        exit(EXIT_FAILURE);
+                    }
+                } else if (strcmp(args[j], "||") == 0) {
+                    if (WEXITSTATUS(status) != 0) {
+                        args[j] = NULL;
+                        execvp(args[0], args);
+                    } else {
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                j++;
+            }
+
+            if (execvp(args[0], args) == -1) {
+                fprintf(stderr, "Command not found\n");
+            }
+            exit(EXIT_FAILURE);
+        } else {
+            wait(&status);
+        }
+    }
+
+    return 0;
+}
